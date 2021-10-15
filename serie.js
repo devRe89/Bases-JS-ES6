25380422c1e0956a274bafbd3575541e7f3900cd
devRe89 <develop.r89@gmail.com>
2021-10-15 15:29:29 -0300

    Up Serie url

--- serie.js
+++ serie.js
@@ -23,11 +23,14 @@ function getPrimes(max) {
     var sieve = [], i, j, primes = [];
     for (i = 2; i <= max; ++i) {
         if (!sieve[i]) {
+            console.log(i);
+            console.log(sieve[i])
             // i has not been marked -- it is prime
             primes.push(i);
             for (j = i << 1; j <= max; j += i) {
-                console.log(j);
+                // console.log(j);
                 sieve[j] = true;
+                console.log(sieve[j]);
             }
         }
     }
@@ -38,3 +41,8 @@ console.time();
 console.log(getPrimes(100));
 // console.log(4 << 1); // 1360
 console.timeEnd();
+
+/*
+https://stackoverflow.com/questions/11966520/how-to-find-prime-numbers-between-0-100
+
+*/
